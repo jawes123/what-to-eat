e9dead5bfee110a9ce5e0a6b4fdfca68c6abed39
James Yu <jamesyu200141@gmail.com>
2023-09-27 18:23:39 -0700

    finished '/add_recipe' route

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 
 const express = require("express");
+const mongoose = require('mongoose');
 const {User, Recipe} = require("./models");
 const app = express();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const upload = multer()
 
 app.post("/add_user", async (request, response) => {
     const newUser = new User({
@@ -30,17 +31,10 @@ app.get("/users/:email", async (request, response) => {
 
   
 app.post("/add_recipe", upload.single('image'), async (request, response) => {
-    //if(request.body.email)
-    //const { user, isLoading } = useAuth0();
     const res = await fetch("http://localhost:3001/users/jamesyu0141@gmail.com")
     const resJSON = res.json();
-    const userJSON = resJSON[0];
-    // fetch("http://localhost:3001/users")
-    // .then((res) => {return res.json()})
-    // .then((userJSONObject) => {
-    //   setUserJSON(userJSONObject[0])})
     resJSON.then(async function(userJSONObj) {
-      let userJSON = userJSONObj[0]; 
+      let userJSON = userJSONObj[0];
       const newRecipe = new Recipe({
         name: request.body.name,
         description: request.body.description,
@@ -48,11 +42,15 @@ app.post("/add_recipe", upload.single('image'), async (request, response) => {
         recipe: request.body.recipe,
         image: request.file
      });
+
+    const updated = await User.findOneAndUpdate({email:userJSON.email}, {$push: {recipes: newRecipe}}, {
+      new:true,
+     });
+
+     console.log(User.storageSize())
   
      try {
-       //await newRecipe.save();
-       //response.send(request.file)
-       response.send(newRecipe);
+       response.send(updated);
      } catch (error) {
        response.status(500).send(error);
      }
